b0ad1f99bc756e176f50aea3e61d9e6199310978
Tom <tomniesse@gmail.com>
2020-03-17 21:30:30 +0100

    html code opgeschoond + 2 nieuwe airco-commandos erin gezet. database-gerelateerde zaken moeten nog

--- cgi-bin-map/src/main.c
+++ cgi-bin-map/src/main.c
@@ -8,37 +8,26 @@
 #include "parse_webrequest.c"
 #include "html.c"
 
-/* Include html-server */
-/*#include "html.c"*/
-
 int main(int argc, char** argv, char** env) {
+	/*
+	 * Parse all GET-requests (if any)
+	 */
 	int index = 0;
 	char getrequest_buffer[MAX_STRING_LENGTH] = {'\0'};
 	/* Create two string srrays for the parsing of get requests */
 	char params[MAX_ARRAY_LENGTH][MAX_STRING_LENGTH];
 	char values[MAX_ARRAY_LENGTH][MAX_STRING_LENGTH];
-
-	printf("Content-type: text/plain\r\n\n");
 	while(env[index] != NULL) {
-		printf("%d: %s\n", index, env[index]);
 		if(strstr(env[index], "REQUEST_URI") != NULL) {
-			printf("GEVIND\n");
 			strncpy(getrequest_buffer, env[index], strlen(env[index]));
-			printf("%s\n", getrequest_buffer);
 			parse_webrequest(getrequest_buffer, params, values);
+			break;
 		}
 		index++;
 	}
-	printf("\nNow for debugging:\n\n");
-	for(index = 0; index < MAX_ARRAY_LENGTH; index++) {
-		if(strlen(params[index]) != 0) {
-			printf("\tPARAM '%s' has VALUE '%s'\n", params[index], values[index]);
-		} else {
-			printf("\nEnd of get request input\n");
-			break;
-		}
-	}
-	return 0;
+	/*
+	 * Render webpage
+	 */
 	printf("Content-type: text/html\r\n\n");
 	print_html_page_contents("html_root/index.html");
 	return 0;
