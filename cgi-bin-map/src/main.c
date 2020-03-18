4c27307599b05d004c01aeef29ae81d55b282006
Tom Niesse <25818005+TomNiesse@users.noreply.github.com>
2020-03-18 15:14:36 +0100

    Update main.c
    klein stukje commentaar toegevoegd

--- cgi-bin-map/src/main.c
+++ cgi-bin-map/src/main.c
@@ -12,8 +12,8 @@
 #define MYSQL_PASSWORD   "domotica"
 #define MAX_QUERY_LENGTH 512
 
-#define DEVICE_ID_AIRCO 2 /* Dit moet nog worden veranderd in het eindproduct. Daar moet het id nummer 3 zijn, de laatste van de 4 apparaten */
-#include "devices/airco.c"
+#define DEVICE_ID_AIRCO 2     /* Dit moet nog worden veranderd in het eindproduct. Daar moet het id nummer 3 zijn, de laatste van de 4 apparaten */
+#include "devices/airco.c"    /* airco.c kan met de database praten om waarden op te halen en bij te werken */
 
 #include "commands.c"
 #include "parse_webrequest.c"
